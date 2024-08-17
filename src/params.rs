8d7949e64cbb13c95f03cb92c45ae7e2491efd1b
Jiatong Zhu <zhujiatong@hotmail.com>
2024-08-17 18:35:52 +0800

    implement `load_safetensors`

--- src/params.rs
+++ src/params.rs
@@ -20,16 +20,42 @@ pub struct LLamaParams<T> {
     pub lm_head: Tensor<T>,   // (vocab_size, dim)
 }
 
+// implementation inspired by @GYHPCG
 impl LLamaParams<f32> {
     pub fn from_safetensors(safetensor: &SafeTensors, config: &LlamaConfigJson) -> Self {
-        todo!("实现从safetensors文件的模型参数加载");
-        // let get_tensor: impl Fn(&str) -> Tensor<f32> = |name: &str| {
-        // ...    
-        // };
-        
-        // LLamaParams {
-        //     embedding_table: get_tensor(...),
-        //     ...
-        // }
+        let get_tensor = |name: &str| -> Tensor<f32> {
+            let tensor = safetensor.tensor(name).unwrap();
+            let data = tensor.data();
+            let shape = tensor.shape();
+
+            let f32_data = data
+                .chunks(4)
+                .map(|x| f32::from_le_bytes(x.try_into().unwrap()))
+                .collect::<Vec<f32>>();
+
+            return Tensor::new(f32_data, &shape.to_vec());
+        };
+
+        let get_layer_tensors = |suffix: &str, layers_count: usize| -> Vec<Tensor<f32>> {
+            return (0..layers_count)
+                .map(|i| get_tensor(&format!("model.layers.{}.{}", i, suffix)))
+                .collect();
+        };
+
+        let num_layers = config.num_hidden_layers;
+        LLamaParams {
+            embedding_table: get_tensor("lm_head.weight"),
+            rms_att_w: get_layer_tensors("input_layernorm.weight", num_layers),
+            wq: get_layer_tensors("self_attn.q_proj.weight", num_layers),
+            wk: get_layer_tensors("self_attn.k_proj.weight", num_layers),
+            wv: get_layer_tensors("self_attn.v_proj.weight", num_layers),
+            wo: get_layer_tensors("self_attn.o_proj.weight", num_layers),
+            rms_ffn_w: get_layer_tensors("post_attention_layernorm.weight", num_layers),
+            w_up: get_layer_tensors("mlp.up_proj.weight", num_layers),
+            w_gate: get_layer_tensors("mlp.gate_proj.weight", num_layers),
+            w_down: get_layer_tensors("mlp.down_proj.weight", num_layers),
+            rms_out_w: get_tensor("model.norm.weight"),
+            lm_head: get_tensor("lm_head.weight"),
+        }
     }
 }
